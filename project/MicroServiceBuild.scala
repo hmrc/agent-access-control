d18b22c603731f416878ebefb80a827437787d0f
Francis Devereux <francis.devereux@digital.hmrc.gov.uk>
2016-09-22 17:37:57 +0100

    [FD][APB-44] Upgrade dependencies

--- project/MicroServiceBuild.scala
+++ project/MicroServiceBuild.scala
@@ -19,13 +19,13 @@ private object AppDependencies {
   import play.core.PlayVersion
 
   private val microserviceBootstrapVersion = "4.4.0"
-  private val playAuthVersion = "3.1.0"
+  private val playAuthVersion = "3.3.0"
   private val playHealthVersion = "1.1.0"
   private val playJsonLoggerVersion = "2.1.1"
-  private val playUrlBindersVersion = "1.0.0"
-  private val playConfigVersion = "2.0.1"
+  private val playUrlBindersVersion = "1.1.0"
+  private val playConfigVersion = "2.1.0"
   private val domainVersion = "3.7.0"
-  private val hmrcTestVersion = "1.6.0"
+  private val hmrcTestVersion = "1.8.0"
 
   val compile = Seq(
     ws,
