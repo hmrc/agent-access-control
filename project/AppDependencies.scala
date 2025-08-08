24a77567170697e240f7d898e1850f0f18fa8980
mrolfe88 <52818407+mrolfe88@users.noreply.github.com>
2025-08-08 15:02:52 +0100

    [APB-8138] Bump bootstrap version

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,7 +1,7 @@
 import sbt.*
 
 object AppDependencies {
-  private val bootstrapVer = "9.16.0"
+  private val bootstrapVer = "9.19.0"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc" %% "bootstrap-backend-play-30" % bootstrapVer,
